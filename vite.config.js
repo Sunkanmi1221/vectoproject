4dc22b9787255176a80f8607acab21cfcaf63f0b
Sunkanmi <belloolasunkanmi2021@gmail.com>
2025-03-15 08:05:36 -0700

    Deploy to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/vectoproject/', 
+//   "homepage": "./"
+// });
+
+
 export default defineConfig({
-  plugins: [react()],
-  base: '/vectoproject/', // Replace with your repository name
+  base: './', // Set relative base path for GitHub Pages
+  plugins: [],
 });
