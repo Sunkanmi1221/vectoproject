843afd55ecfdb15c6c0c89be03adfd4e265fa83d
Sunkanmi <belloolasunkanmi2021@gmail.com>
2025-03-19 07:22:59 -0700

    new commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,13 @@
+import React from 'react';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import ReactDOM from 'react-dom/client';
+
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<App />);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
 
 // export default defineConfig({
 //   plugins: [react()],
@@ -7,8 +7,25 @@ import react from '@vitejs/plugin-react';
 //   "homepage": "./"
 // });
 
+// https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
+// export default defineConfig({
+//   base: './', // Set relative base path for GitHub Pages
+//   plugins: [],
+// });
+
+
+// export default defineConfig({
+//   base: '/vectoproject/', // Match your repo name
+//   })
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: './', // Set relative base path for GitHub Pages
-  plugins: [],
+  plugins: [react()],
 });
