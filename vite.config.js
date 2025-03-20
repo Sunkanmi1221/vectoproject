4eee6b519cf4d65ba2dd153ee1267545ea6abd9a
Sunkanmi <belloolasunkanmi2021@gmail.com>
2025-03-20 04:34:50 -0700

    Fix Vite base path and module loading

--- vite.config.js
+++ vite.config.js
@@ -29,5 +29,5 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   // base: '/vectoproject/', // For GitHub Pages
-  base: './', // For Netlify
+  base: './',  // For Netlify
 });
